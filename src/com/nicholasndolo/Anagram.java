a1c972478d2e2a17bd24a0976585069b43c4376e
Nicholas <nickndolo95@gmail.com>
2024-11-12 09:32:24 +0300

    implement anagramDetector method

--- src/com/nicholasndolo/Anagram.java
+++ src/com/nicholasndolo/Anagram.java
@@ -1,5 +1,7 @@
 package com.nicholasndolo;
 
+import java.util.Map;
+import java.util.HashMap;
 public class Anagram {
     public boolean isAnagram(String original, String test){
         if(original.length() != test.length())
@@ -18,5 +20,30 @@ public class Anagram {
         }
         return true;
     }
+
+ public boolean anagramDetector(String original, String test){
+     if(original.length() != test.length())
+         return false;
+
+     Map<Character, Integer> map = new HashMap<>();
+
+     for(char ch : original.toLowerCase().toCharArray()){
+         var count = map.getOrDefault(ch, 0);
+         map.put(ch, count + 1);
+     }
+
+     for(char ch : test.toLowerCase().toCharArray()){
+         if(map.get(ch) == null || map.get(ch) == 0)
+             return false;
+
+         var count = map.get(ch);
+         map.put(ch, count - 1);
+
+     }
+
+     return true;
+    }
+
+
 
 }
